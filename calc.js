aca364f26c9d845b611b39dafd5d92215543a36c
nassim <bladess@hotmail.fr>
2017-07-04 14:21:38 +0200

    fonction add/mult

--- calc.js
+++ calc.js
@@ -1,4 +1,21 @@
 var nombreUn =Number(prompt("entrez un premier nombre"));
 var nombreDeux =Number(prompt("entrez un second nombre"));
 
-alert("la somme de vos nombres est : " + (nombreUn+ nombreDeux)+" \n le produit de vos nombres est : "+ (nombreUn* nombreDeux));
+//alert("la somme de vos nombres est : " + add(nombreUn,nombreDeux)+" \nLe produit de vos nombres est : "+ mult(nombreUn,nombreDeux));
+
+var monTitre = document.querySelector('h1');
+monTitre.textContent = "La somme de vos nombres est : " + add(nombreUn,nombreDeux);
+
+var monTitre2 = document.querySelector('h2');
+monTitre2.textContent = "Le produit de vos nombres est : "+mult(nombreUn,nombreDeux);
+
+//document.querySelector('html').onclick = function() {
+  //  alert('Aïe, arrêtez de cliquer !!');
+//}
+
+function add(n1, n2){
+	return n1 + n2;
+}
+function mult(n1, n2){
+	return n1 * n2;
+}
