76b9ab1f3a5a38e5266cdae84031167e403479aa
nassim <bladess@hotmail.fr>
2017-07-04 11:33:10 +0200

    premier attempt calc

---
+++ calc.js
@@ -0,0 +1,4 @@
+var nombreUn =Number(prompt("entrez un premier nombre"));
+var nombreDeux =Number(prompt("entrez un second nombre"));
+
+alert("la somme de vos nombres est : " + (nombreUn+ nombreDeux)+" \n le produit de vos nombres est : "+ (nombreUn* nombreDeux));
